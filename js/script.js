1349a7c23c6bb5db9c8c26de1f2c5f3940172e32
nihalmn028 <nihalmn028@gmail.com>
2023-11-10 20:03:15 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,90 @@
+
+function clickk1(){
+ 
+ document.getElementById('txt').value+=document.getElementById('one').innerHTML
+ 
+ 
+}
+function clickk2(){
+ 
+  document.getElementById('txt').value+=document.getElementById('two').innerHTML
+  
+ }
+ function clickk3(){
+ 
+  document.getElementById('txt').value+=document.getElementById('three').innerHTML
+  
+ }
+ function clickk4(){
+ 
+  document.getElementById('txt').value+=document.getElementById('four').innerHTML
+  
+ }
+ function clickk5(){
+ 
+  document.getElementById('txt').value+=document.getElementById('five').innerHTML
+  
+ }
+ function clickk6(){
+ 
+  document.getElementById('txt').value+=document.getElementById('six').innerHTML
+  
+ }
+ function clickk7(){
+ 
+  document.getElementById('txt').value+=document.getElementById('seven').innerHTML
+  
+ }
+ function clickk8(){
+ 
+  document.getElementById('txt').value+=document.getElementById('eight').innerHTML
+  
+ }
+ function clickk9(){
+ 
+  document.getElementById('txt').value+=document.getElementById('nine').innerHTML
+  
+ }
+ function clickk10(){
+ 
+  document.getElementById('txt').value+=document.getElementById('ten').innerHTML
+  
+ }
+ function clickk11(){
+ 
+  document.getElementById('txt').value+=document.getElementById('eleven').innerHTML
+  
+ }
+ function clickk12(){
+ 
+  document.getElementById('txt').value+=document.getElementById('twelve').innerHTML
+  
+ }
+ function clickk13(){
+ 
+  document.getElementById('txt').value+=document.getElementById('thirteen').innerHTML
+  
+ }
+ function clickk14(){
+ 
+  document.getElementById('txt').value+=document.getElementById('fourteen').innerHTML
+  
+ }
+ function clickk15(){
+ 
+  // document.getElementById('txt').value+=document.getElementById('fifteen').innerHTML
+  a=document.getElementById('txt').value
+  b=eval(a)
+  document.getElementById('txt').value=b
+
+ }
+ function clickk16(){
+ 
+  document.getElementById('txt').value+=document.getElementById('sixteen').innerHTML
+  
+ }
+ function clickk0(){
+ document.getElementById('txt').value=''
+
+ }
+ 
